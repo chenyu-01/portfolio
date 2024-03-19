6180e4372d3004a50e2c0419382304a8be40e317
chenyu-01 <chenyu101@outlook.com>
2024-03-19 18:29:39 +0800

    Add color mode selector component

---
+++ components/color-mode-selector.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex space-x-2 items-center">
+    <div v-if="showNextModeLabel" class="text-xs">Change to {{ nextMode }}</div>
+    <button
+      class="px-2 py-1"
+      @click.prevent="toggleMode"
+      @mouseenter="showNextModeLabel = true"
+      @mouseleave="showNextModeLabel = false"
+    >
+      {{ nextModeIcon }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+const showNextModeLabel = ref(false)
+const colorMode = useColorMode()
+const modes = ['light', 'dark', 'system']
+const nextModeIcons = {
+  light: '🌞',
+  dark: '🌙',
+  system: '🌐',
+}
+const nextMode = computed(() => {
+  const currentIndex = modes.indexOf(colorMode.preference)
+  return modes[(currentIndex + 1) % modes.length]
+})
+
+const nextModeIcon = computed(() => nextModeIcons[nextMode.value])
+
+const toggleMode = () => {
+  colorMode.preference = nextMode.value
+}
+</script>
