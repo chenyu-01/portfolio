91772e4fbd8e8f973103e74b8d6a46ce88e6290b
chenyu-01 <chenyu101@outlook.com>
2024-03-19 16:02:06 +0800

    Fix: tsconfig add nuxt-color module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  typescript: {
+    typeCheck: true,
+  },
 })
